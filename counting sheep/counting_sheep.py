3511e68b6564d8c302113cd79f79c30b85bf7057
Piotr Celej <piotrcelej@interia.pl>
2021-03-02 14:19:23 +0100

    count trues in a table

--- counting sheep/counting_sheep.py
+++ counting sheep/counting_sheep.py
@@ -8,14 +8,16 @@ data = [True,  True,  True,  False,
 missing_data = [True,  True,  True,  False,
   True,  True,  True,  True ,
   True,  False, True,  "null",
-  True,  False, False, True ,
+  True,  False, False, "True" ,
   True,  True,  True,  True ,
   False, False, True,  True]
 
 def count_sheeps(inData) :
     #check proper values
     output = 0
-
+    for i in inData :
+        if type(i) == bool :
+            if i : output += 1  
     return output
 
-print("No of sheeps %i",count_sheeps(data))
+print("No of sheeps: ",count_sheeps(missing_data))
