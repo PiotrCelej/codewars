7d0db4dbe2096afc033a4e272d37032faa0b254a
Piotr Celej <piotrcelej@interia.pl>
2021-03-02 14:11:47 +0100

    Counting sheep frame

---
+++ counting sheep/counting_sheep.py
@@ -0,0 +1,21 @@
+data = [True,  True,  True,  False,
+  True,  True,  True,  True ,
+  True,  False, True,  False,
+  True,  False, False, True ,
+  True,  True,  True,  True ,
+  False, False, True,  True]
+
+missing_data = [True,  True,  True,  False,
+  True,  True,  True,  True ,
+  True,  False, True,  "null",
+  True,  False, False, True ,
+  True,  True,  True,  True ,
+  False, False, True,  True]
+
+def count_sheeps(inData) :
+    #check proper values
+    output = 0
+
+    return output
+
+print("No of sheeps %i",count_sheeps(data))
