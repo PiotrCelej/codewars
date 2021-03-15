c7572bfc6c4949e9ddfe0a120bca05b1943d6be9
Piotr Celej <piotrcelej@interia.pl>
2021-03-15 15:50:52 +0100

    string incrementer

--- string_incrementer.py
+++ string_incrementer.py
@@ -18,5 +18,31 @@ foo099 -> foo100
 Attention: If the number has leading zeros the amount of digits should be considered.
 '''
 
-def stringIncrementer(string) :
-    return ""
+def stringIncrementer(strng) :
+    outTab = ["",""]
+    nums = "0123456789"
+    l_num = True
+    for i in reversed(strng) :
+        if i in nums and l_num :
+            outTab[1] = outTab[1] + i
+        else :
+            outTab[0] = outTab[0] + i
+            l_num = False
+    word = ""
+    nmbr = ""
+    for j in reversed(outTab[0]) :
+        word += j
+    for k in reversed(outTab[1]) :
+        nmbr +=k
+    n=1
+    if len(nmbr)>0 :
+        n = str(int(nmbr)+1)
+
+    format_string = "{:0>"+str(len(nmbr))+"}"
+    return word+format_string.format(n)
+
+print(stringIncrementer('foo12'))
+print(stringIncrementer('foo0043'))
+print(stringIncrementer('foo100'))
+print(stringIncrementer('foobar0001'))
+print(stringIncrementer('foo23bar0004'))
