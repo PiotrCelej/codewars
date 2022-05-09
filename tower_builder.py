424186ab8863afe2f11218dbf67cef8ea032f123
Piotr Celej <piotrcelej@interia.pl>
2022-05-09 16:14:05 +0200

    tower builder

--- tower_builder.py
+++ tower_builder.py
@@ -1,5 +1,25 @@
 
+
 def tower_builder(n_floors) :
     if n_floors <=0 : return []
 
+    #count number of stars on each floor
+    stars = [1]
+    for i in range(n_floors-1) :
+        stars.append(stars[i]+2)
+
+    #max length of a floor is in the last element
+    max_stars = stars[len(stars)-1]
     
+    result = []
+    for i in range(n_floors) :
+        spaces = int((max_stars-stars[i])/2)
+        row_spaces = ''.join(' ' for j in range(spaces))
+        row_stars = ''.join('*' for k in range(stars[i]))
+        row = row_spaces+row_stars+row_spaces
+        result.append(row)
+
+    return result
+
+    
+print(tower_builder(3))
